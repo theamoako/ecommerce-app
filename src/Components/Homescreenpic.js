49de7a2615c23fa9b54071fdfb0089f8c55bfa99
Kofi <AmoakoJaden@gmail.com>
2022-09-03 22:36:51 +0000

    second commit

--- src/Components/Homescreenpic.js
+++ src/Components/Homescreenpic.js
@@ -6,14 +6,41 @@ const Homescreenpic = () => {
     return (
         <div className="onboardscreen" >
         <div className="onboard1" >
+            <div>
            <img src={background} alt="onboardsceenpic" className="pic2"/> 
+           <div className="head2"><h1 className="head">E-FOUNTAIN</h1></div>
+             <div className="sloga2">Place to buy and sell</div>
+           </div>
          <div className="items" align="center">
             <div className="header" align="center">
-            <div className="head2"><h1 className="head">E-FOUNTAIN</h1></div>
-             <div className="sloga2">Place to buy and sell</div>
+           
             </div>   
          </div>
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <br></br>
+        <br></br>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <br></br>
+        <br></br>
+        <br></br>
         <div className="items2">
             <h1>div</h1>
         </div>
