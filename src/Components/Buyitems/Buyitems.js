297a91244b6b176dd58e44af49d74fac59488cf5
Kofi <AmoakoJaden@gmail.com>
2022-09-06 12:23:18 +0000

    second commit

--- src/Components/Buyitems/Buyitems.js
+++ src/Components/Buyitems/Buyitems.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Buyitems = () => {
     return (
-       <></>
+       <header>
+        <h1>
+            E-fountain
+        </h1>
+       </header>
     );
 }
 
