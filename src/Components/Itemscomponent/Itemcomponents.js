8f63d077a41a9d4455326730dce248eecbf2a16b
Kofi <AmoakoJaden@gmail.com>
2022-09-07 00:47:03 +0000

    second commit

---
+++ src/Components/Itemscomponent/Itemcomponents.js
@@ -0,0 +1,23 @@
+import React from "react";
+import test from '.../images/headphones.jpg'
+
+const Itemscomponent = () => {
+    return (
+        <div className="movie" key={imdbID}>
+          <div>
+            <p>2018</p>
+          </div>
+    
+          <div>
+            <img src={test} alt={Title} />
+          </div>
+    
+          <div>
+            <span>45</span>
+            <h3>phone</h3>
+          </div>
+        </div>
+      );
+}
+
+export default Itemscomponent;
